386227a3d54228f50dbcdf6cade25607c467ed9e
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-05 16:58:19 +0200

    Types

---
+++ language basics/Types/index.js
@@ -0,0 +1,24 @@
+// var a = 5
+
+// console.log(a)
+
+// var a = 20
+
+// console.log(a)
+
+var array = [2, 3, 5]
+
+var anotherArray = array
+
+console.log(array)
+console.log(anotherArray)
+
+
+array.pop(1)
+array.push(6)
+
+
+array.join(1)
+
+console.log(array)
+console.log(anotherArray)
