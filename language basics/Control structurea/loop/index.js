49fbd3b6db917ade49d0c35c1b2e8de5b9719eff
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-05 16:30:17 +0200

    control strutures loop- for, while

---
+++ language basics/Control structurea/loop/index.js
@@ -0,0 +1,18 @@
+
+
+// const array = [1, 3, 5, 7, 9]
+
+// for (let index = 2; index < array.length; index++) {
+//   const element = array[index];
+
+//   console.log(element)
+  
+// }
+
+let index =0
+
+while (index<=10 ) {
+  index++;
+
+  console.log(index)
+}
