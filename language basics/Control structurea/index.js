6ab8213baeeb15cd8f75e8a4a55203a7ec26e467
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-05 16:21:30 +0200

    control strutures

---
+++ language basics/Control structurea/index.js
@@ -0,0 +1,11 @@
+// If and else
+
+var numerber = 3
+
+if (numerber > 34) {
+  console.log('Numero maior')
+} else if (numerber === 34) {
+  console.log('Os numero sao iguais')
+} else {
+  console.log('Numero Menor')
+}
