57d3ba48a107641b468d5ce9eacd0641549238ef
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-05 16:24:34 +0200

    control strutures switch

---
+++ language basics/Control structurea/Switch/index.js
@@ -0,0 +1,23 @@
+
+var n = 3
+
+
+switch (n) {
+  case 1:
+    console.log('Is 1')
+    break
+
+  case 2:
+    console.log('Is 2')
+    break
+
+  case 3:
+    console.log('Is 3')
+    break
+
+  case 4:
+    console.log('Is 4')
+    break
+  default:
+    break
+}
