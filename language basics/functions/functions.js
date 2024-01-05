1f7a2d87aefbd1452a07f43377d56ca0f3222ecf
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-05 12:35:11 +0200

    Functions

---
+++ language basics/functions/functions.js
@@ -0,0 +1,21 @@
+function Myname() {
+  return 'Fernando dos Santos'
+}
+
+console.log(Myname())
+
+// With paramenters
+
+function calcul(number1, number2) {
+  return number1 + number2
+}
+
+console.log(calcul(10, 5))
+
+// Arrow Functions
+
+const Mult = (n1, n2) => {
+  return n1 * n2
+}
+
+console.log(Mult(4, 5))
