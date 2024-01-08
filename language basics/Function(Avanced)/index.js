3225dabf31caf17ab20bd3da9fd7048b518bb64c
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-08 16:11:05 +0200

    Function advanced update

---
+++ language basics/Function(Avanced)/index.js
@@ -0,0 +1,31 @@
+// function generator(input) {
+//   var number = input
+
+//   return function () {
+//     return number * 2
+//   }
+// }
+
+// var calc = generator(9)
+// console.log(calc())
+
+// Call imediat funcion
+// ;(function calc() {
+//   console.log('calc')
+// })()
+
+// setTimeout(function () {
+//   console.log('Finished')
+// }, 2000)
+
+// var interval = setInterval(() => {
+//   console.log('Finished')
+// }, 500)
+
+// setTimeout(function () {
+//   clearInterval(interval)
+// }, 2500)
+
+var a = 24.2234
+
+console.log(a.toFixed(1))
