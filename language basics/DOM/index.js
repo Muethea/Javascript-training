12a1ddec296fd762cd8f680b3fbb5e4e203011e1
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-08 19:10:42 +0200

    DOM JS

---
+++ language basics/DOM/index.js
@@ -0,0 +1,35 @@
+var h1 = document.getElementById('h1')
+var inputValue = document.getElementById('nome')
+var valorEsperado = Math.floor(Math.random() * 9000) + 1000
+const button = document.getElementById('button')
+
+h1.style.background = 'red'
+h1.style.padding = '13px'
+h1.style.color = '#ffff'
+
+button.addEventListener('click', () => {
+  const valorDigitado = inputValue.value.trim()
+  if (valorDigitado === '' || valorDigitado < 4) {
+    h1.innerHTML = 'Por favor digite um valor de quatro digito'
+    return
+  }
+  if (valorDigitado > 9999) {
+    h1.innerHTML = 'Por favor digite um valor de quatro digito'
+    return
+  }
+
+  if (valorDigitado == valorEsperado) {
+    h1.innerHTML = 'Acertou'
+  } else {
+    let feedback = ''
+
+    if (Math.abs(valorDigitado - valorEsperado) <= 100) {
+      feedback = 'Voce esta proximo'
+    } else if (valorDigitado > valorEsperado) {
+      feedback = 'Muito menor'
+    } else {
+      feedback = 'Muito maior'
+    }
+    h1.innerHTML = `Tente novamente ${feedback} que o numero digitado. o valor correto era ${valorEsperado}`
+  }
+})
