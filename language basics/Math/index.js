54083b871b20fa2eb6daee21c1a4b43af3dea475
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-08 16:09:43 +0200

    Math object

---
+++ language basics/Math/index.js
@@ -0,0 +1,27 @@
+// var pi = Math.PI
+
+// console.log(pi)
+
+// var e = Math.E
+
+// console.log(e)
+
+// var numero = 2
+
+// numero = Math.pow(numero, 2)
+
+// console.log(numero)
+
+// Random number
+
+// var randon = Math.floor(Math.random() * 100) + 1
+
+// var number = 93
+
+// if (number == randon) {
+//   console.log('Numero iguais')
+//   console.log(randon)
+// } else {
+//   console.log('numero nao e igual tente novamente')
+//    console.log(randon)
+// }
