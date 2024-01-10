2f050b2278a7acc7144ec3bd37e5ca5f60dfe0cb
Fernando dos Santos Muethea <fmuethea4@gemail.com>
2024-01-10 16:18:31 +0200

    feat:events

---
+++ language basics/Events/index.js
@@ -0,0 +1,12 @@
+var button = document.querySelector('button')
+
+// Add an event listener to the button
+// button.addEventListener('click', function () {
+//   alert('clicado')
+// })
+
+
+
+button.addEventListener('click', function () {
+  alert('clicado')
+})
